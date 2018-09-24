9522aabe08b7a2cf5e9b954ff9788e152e3db0f2
diwao <idNnNru26QHv>
2018-09-24 11:53:35 +0900

    update

--- dev-omikuji/src/omikuji.js
+++ dev-omikuji/src/omikuji.js
@@ -1,11 +1,11 @@
 class Omikuji {
   // 配列で「大吉」や「中吉」などの値を設定
-  constructor(array) {
+  constructor (array) {
     this.items = array
   }
 
   // 設定した値をランダムで取得
-  get() {
+  get () {
     const rand = Math.floor(Math.random() * this.items.length)
     return this.items[rand]
   }
